192f0e77fb4435ca43b426792bf0eef9a10248a1
kgni <admin@mdia.dk>
2022-09-11 12:16:35 +0200

    add passport and controller for GET login and POST login

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const connectDB = require('./config/database');
+const flash = require('express-flash');
 const morgan = require('morgan');
 
 // routes
@@ -15,11 +20,27 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(morgan('tiny'));
+
+// Sessions
+app.use(
+	session({
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized: false,
+		store: MongoStore.create({ mongoUrl: process.env.MONGO_DB_URI }),
+		touchAfter: 24 * 3600, // time period in seconds
+	})
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use('/', mainRoutes);
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server is running on port ${process.env.PORT}`);
 });
-
-app.use('/', mainRoutes);
